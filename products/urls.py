be5e3d462426c7a8b33c8ea63f816354e4c8801f
Muneer Rahman <muneerrahman010@gmail.com>
2025-12-27 16:48:25 +0530

    add urls for fbv

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ProductListCreateView, ProductRetrieveUpdateDeleteView,product_index
+from .views import product_list_create,product_retrieve_update_delete,product_index
 
 urlpatterns = [
-    path('products/', ProductListCreateView.as_view()),
-    path('products/<int:pk>/', ProductRetrieveUpdateDeleteView.as_view()),
+    path('products/', product_list_create),
+    path('products/<int:pk>/', product_retrieve_update_delete),
     path("ui/", product_index),
 ]
 
